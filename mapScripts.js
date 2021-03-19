fe72320341e3287c5ab352843f7f6068be2ead7d
Saiveer1996 <77888103+Saiveer1996@users.noreply.github.com>
2021-03-19 03:26:49 -0600

    Add files via upload

---
+++ mapScripts.js
@@ -0,0 +1,123 @@
+
+const map = L.map('leafletMap', {
+  center: [51.0447, -114.0719],
+  zoom:10.5
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/saiveer1996/ckmfx8nrrapmk17o7fbonj1ce/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2FpdmVlcjE5OTYiLCJhIjoiY2ttNXlsb2gyMGl4ZzJvcXNremMzYThuciJ9.gHCefOrtQl5IyKsre0lKKw'
+
+
+
+}).addTo(map);
+
+function collectData() {
+
+    const requestURL = "https://data.calgary.ca/resource/fd9t-tdn2.geojson";
+    const requestURLClinics = "https://data.calgary.ca/resource/x34e-bcjz.geojson?$where=type == 'PHS Clinic' or type == 'Hospital'";
+
+    const schoolData = new HttpClient();
+    schoolData.get(requestURL, function(response) {
+      createMarkers(response, "schools");
+    });
+
+    const clinicData = new HttpClient();
+    clinicData.get(requestURLClinics, function(response) {
+      createMarkers(response, "hospitals");
+    });
+
+}
+const schoolIcon = L.divIcon({className: 'schoolIcon'});
+const hospitalIcon = L.divIcon({className: 'hospitalIcon'});
+const hospitalIconClicked = L.divIcon({className: 'hospitalIconClicked'});
+
+let schools = L.featureGroup();
+let hospitals = L.featureGroup();
+let nearestHospitalPoint = L.featureGroup();
+
+L.control.layers(null,
+  {
+    "Schools": schools,
+    "Hospitals": hospitals
+  }, {
+  collapsed: false
+}).addTo(map);
+
+map.addLayer(schools);
+map.addLayer(hospitals);
+
+map.addLayer(nearestHospitalPoint);
+
+schools.on("click", function (e) {
+
+  nearestHospitalPoint.clearLayers();
+
+  const clickedMarker = e.layer.toGeoJSON();
+
+  const nearestHospital = turf.nearest(clickedMarker, hospitals.toGeoJSON());
+
+  const nhLng = nearestHospital.geometry.coordinates[0];
+
+  const nhLat = nearestHospital.geometry.coordinates[1];
+
+  L.marker([nhLat, nhLng], {icon: hospitalIconClicked, zIndexOffset: 1000}).addTo(nearestHospitalPoint);
+
+  document.getElementById('announcer').style.display = "block";
+  document.getElementById('schoolName').innerHTML = e.layer.options.title;
+  document.getElementById('kmDistance').innerHTML = nearestHospital.properties.distanceToPoint.toFixed(3);
+
+  hospitals.eachLayer(function (e) {
+    const hLat = e._latlng.lat.toFixed(6);
+    const hLng = e._latlng.lng.toFixed(6);
+
+    if (hLat == nhLat && hLng == nhLng) {
+      document.getElementById('hospName').innerHTML = e.options.title;
+    }
+  });
+});
+
+let HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        let anHttpRequest = new XMLHttpRequest();
+
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.open( "GET", aUrl, true );
+
+        anHttpRequest.setRequestHeader("X-App-Token", "0Yy2rHqfsSy863vVSti73hwb7");
+        anHttpRequest.send();
+    }
+}
+
+function createMarkers(json, type) {
+  const data = JSON.parse(json);
+
+  if (data.features.length == 0) {
+    alert("Sorry");
+  } else {
+    for (i in data.features) {
+
+      if (data.features[i].geometry != null) {
+
+        let coords = data.features[i].geometry.coordinates;
+
+        const name = data.features[i].properties.name || "N/A";
+
+        let marker = new L.marker([coords[1], coords[0]], {title: name}).bindPopup(name);
+
+        if (type == "hospitals") {
+          marker.setIcon(hospitalIcon).addTo(hospitals);
+        } else if (type == "schools") {
+          marker.setIcon(schoolIcon).addTo(schools);
+        }
+
+      }
+    }
+  }
+}
